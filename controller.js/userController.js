20af1678bdfac7b3919977eabd37873dae26f06c
Vinayak Chavan <vinayakchavan@credenceanalytics.com>
2022-06-11 01:50:03 +0530

    token generation for login and register done

--- controller.js/userController.js
+++ controller.js/userController.js
@@ -4,7 +4,7 @@ const User = require("../DbSchema/userSchema");
 
 //Register
 const register = async (req, res) => {
-    if(!req.body.fname || !req.body.lname || !req.body.email || !req.body.password){
+    if (!req.body.fname || !req.body.lname || !req.body.email || !req.body.password) {
         return res.status(400).send(`Please fill all the field.`)
     }
     // user already register then 
@@ -15,21 +15,51 @@ const register = async (req, res) => {
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = bcrypt.hash(req.body.password, salt);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
 
     //Create new user
     const newUser = new User({
-        fname : req.body.fname,
-        lname : req.body.lname,
-        email : req.body.email,
-        password : req.body.password
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        password: hashPassword
     });
     try {
         const saveUser = await newUser.save();
-        return res.status(200).send("user created Successfully.")        
+        const token = jwt.sign({ id: saveUser._id }, process.env.TOKEN_SECRET, { expiresIn: '5d' });
+        return res.status(201).send({ "status": "success", "msg": "user created Successfully.", "token": token })
     } catch (err) {
+        console.log(err)
         res.status(500).send(err);
     }
 }
 
-module.exports = { register }
+//Login User
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (email && password) {
+            const user = await User.findOne({ email: email });
+            if (user) {
+                console.log(user);
+                const validPass = await bcrypt.compare(password, user.password);
+                if (user.email === email && validPass) {
+                    //Generate Token 
+                    const token = await jwt.sign({ id: user._id }, process.env.TOKEN_SECRET, { expiresIn: '5d' });
+                    res.send({ "status": "success", "msg": "Login success.", "token":token})
+                } else {
+                    res.send({ "status": "unsuccess", "msg": "Email or password is not valid" });
+                }
+            } else {
+                res.send({ "status": "unsuccess", "msg": "Your not Register User." });
+            }
+        }else{
+            res.send({ "status": "unsuccess", "msg": "All field are require." })
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(error);
+    }
+}
+module.exports = { register, login }
--- routes/authServer.js
+++ routes/authServer.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-const { register } =  require("../controller.js/userController");
+const { register, login } =  require("../controller.js/userController");
 
 
 router.post('/register', async (req,res)=> {
-    const data = await register(req, res);
+    try {
+        const data = await register(req, res);
+    } catch (err) {
+        res.status(500).send(`Internal error`);
+    }
+});
+
+router.post('/login', async (req,res)=>{
+    try {
+        const loginResp = await login(req,res);
+    } catch (err) {
+        res.status(500).send(`Internal error`);
+    }
 });
 
 module.exports = router;
