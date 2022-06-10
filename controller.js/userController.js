83fb69aaeef1519241c1aa57e5066c83b9e4e229
Vinayak Chavan <vinayakchavan@credenceanalytics.com>
2022-06-11 02:14:23 +0530

    res msg change.

--- controller.js/userController.js
+++ controller.js/userController.js
@@ -4,24 +4,25 @@ const User = require("../DbSchema/userSchema");
 
 //Register
 const register = async (req, res) => {
-    if (!req.body.fname || !req.body.lname || !req.body.email || !req.body.password) {
-        return res.status(400).send(`Please fill all the field.`)
+    const { fname, lname, email, password } = req.body;
+    if (!fname || !lname || !email || !password) {
+        return res.status(400).send({ "status": "unsuccess", "msg": "All field are mandatory.", "token": token })
     }
     // user already register then 
-    const emailExist = await User.findOne({ email: req.body.email });
+    const emailExist = await User.findOne({ email: email });
     if (emailExist) {
         console.log(req.body.email);
-        return res.status(400).send(`Email already exist.`);
+        return res.status(409).send({ "status": "success", "msg": "User already with same email .", "token": token });
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const hashPassword = await bcrypt.hash(password, salt);
 
     //Create new user
     const newUser = new User({
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
+        fname: fname,
+        lname: lname,
+        email: email,
         password: hashPassword
     });
     try {
@@ -29,7 +30,6 @@ const register = async (req, res) => {
         const token = jwt.sign({ id: saveUser._id }, process.env.TOKEN_SECRET, { expiresIn: '5d' });
         return res.status(201).send({ "status": "success", "msg": "user created Successfully.", "token": token })
     } catch (err) {
-        console.log(err)
         res.status(500).send(err);
     }
 }
@@ -59,7 +59,7 @@ const login = async (req, res) => {
         }
     } catch (err) {
         console.log(err)
-        res.status(500).send(error);
+        res.status(500).send(err);
     }
 }
 module.exports = { register, login }
